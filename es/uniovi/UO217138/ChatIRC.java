8c28b7e75f3641fa935de2d1852f596f8e88c698
Óscar de Arriba <odarriba@gmail.com>
2013-04-02 13:44:27 +0200

    Commit inicial

---
+++ es/uniovi/UO217138/ChatIRC.java
@@ -0,0 +1,24 @@
+/*
+ * Cliente de chat IRC-like
+ * Trabajo grupal de Computadores
+ * 
+ * Clase ChatIRC
+ * 
+ * Autores:
+ *  - Lucas çlvarez
+ *  - îscar de Arriba
+ *  - Estefan’a Gonz‡lez
+ */
+package es.uniovi.UO217138;
+
+public class ChatIRC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
